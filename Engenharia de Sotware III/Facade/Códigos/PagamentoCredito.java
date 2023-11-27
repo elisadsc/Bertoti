5688fca4d02a245036bddbec20a63004c339c590
Elisa Carvalho <99971201+elisadsc@users.noreply.github.com>
2023-11-26 22:53:38 -0300

    Add files via upload

---
+++ Engenharia de Sotware III/Facade/Códigos/PagamentoCredito.java
@@ -0,0 +1,5 @@
+class PagamentoCredito {
+    public void realizarPagamentoCredito(double valor) {
+        System.out.println("Pagamento com cartão de crédito no valor de R$" + valor);
+    }
+}
---
+++ Engenharia de Sotware III/Facade/Códigos/PagamentoDebito.java
@@ -0,0 +1,5 @@
+class PagamentoDebito {
+    public void realizarPagamentoDebito(double valor) {
+        System.out.println("Pagamento com cartão de débito no valor de R$" + valor);
+    }
+}
